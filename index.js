c2f3003f47f41aa85690969c7f187e74fc78c758
Zyanya Ahuachtli Zacatenco Pedroza <230852@utxicotepec.edu.mx>
2024-11-22 17:21:28 -0600

    Práctica 09
    Creación de los archivos index.js y saluda.js, así como configuración del Node.js (se crearon los archivos package-lock.json y package.json, configurándose éste último para que funcione con el index.js.

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require('express'); //Declaración que permitirá entrar al protocolo http y leer páginas. Importar la librería para crear un servidor web
+
+//Instanciar nuestra aplicación web
+const app=express();
+
+//Configuramos nuestro servidor web 
+const port=3000;
+app.listen(port, ()=>{
+    console.log(`La aplicación ha iniciado en el puerto: ${port}`); //Se levanta el servidor
+});
+
+//Probamos las rutas para poder presentar mensajes al usuario a través del navegador
+app.get("/", function(req, res){
+    res.send("Hola Mundo desde Node, a través del Navegador.");
+});
+
+app.get("/QuienSoy", function(req, res){
+    res.json({"estudiante": "Zyanya Ahuachtli Zacatenco Pedroza",
+        "carrera": "TI DSM",
+        "grado": "4°",
+        "grupo": "B",
+        "asignatura": "Aplicaciones Web Orientada a Servicios (AWOS)"
+    });
+});
